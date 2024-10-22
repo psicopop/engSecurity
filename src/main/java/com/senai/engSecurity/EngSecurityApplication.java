a36eeec4782e0be22de6b4da08b556b7dccd5c58
jonatas.oliveira <jonatas.oliveira@fornecedores.goias.gov.br>
2024-10-22 20:33:18 -0300

    Comentario

--- src/main/java/com/senai/engSecurity/EngSecurityApplication.java
+++ src/main/java/com/senai/engSecurity/EngSecurityApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EngSecurityApplication {
 
+//	Turma de Engenharia de Software 6
 	public static void main(String[] args) {
 		SpringApplication.run(EngSecurityApplication.class, args);
 	}
